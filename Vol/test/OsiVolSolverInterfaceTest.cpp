0d602a1ae726e920f0576411a01195c2c35d4f5b
Stefan Vigerske <svigerske@gams.com>
2011-06-12 16:43:48 +0000

    update OsiVol unittest

--- Vol/test/OsiVolSolverInterfaceTest.cpp
+++ Vol/test/OsiVolSolverInterfaceTest.cpp
@@ -3,18 +3,9 @@
 // Corporation and others.  All Rights Reserved.
 // This code is licensed under the terms of the Eclipse Public License (EPL).
 
-#ifdef NDEBUG
-#undef NDEBUG
-#endif
-
-#include <cassert>
-
 #include "OsiVolSolverInterface.hpp"
 
-//#############################################################################
-
 //--------------------------------------------------------------------------
-// test EKKsolution methods.
 int
 OsiVolSolverInterfaceUnitTest(const std::string & mpsDir, const std::string & netlibDir)
 {
